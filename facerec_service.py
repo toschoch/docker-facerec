d65abc97b2415659b3635deda16cfa4121259b1a
Tobias Schoch <Tobias.Schoch@helbling.ch>
2018-04-30 13:36:40 +0200

    added the service api for code and image requests

--- facerec_service.py
+++ facerec_service.py
@@ -1,17 +1,14 @@
 import os
-from os import listdir
-from os.path import isfile, join, splitext
 
 from facerec import facedb, dlib_api
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 import numpy as np
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
+from flask_wtf.file import FileField
 from flask import Flask, jsonify, request, flash, render_template, redirect, url_for
 from wtforms import TextField
-from wtforms.validators import required
 from werkzeug.exceptions import BadRequest
-from werkzeug.utils import secure_filename
+import json
 
 import utils
 
@@ -22,57 +19,57 @@ app = Flask(__name__)
 
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '8da7a0d2-5cec-4cec-99e7-2979768dca67'
-#CORS(app)
 
-# <Picture functions> #
-
-def load_image(fname):
-    return np.array(Image.open(fname, 'r'))
-
-
+# set database path to data dir
 facedb.set_db_path(os.path.join(os.path.split(__file__)[0],'data'))
-# dlib_api.teach_person(load_image("/home/Tobi/Downloads/IMG_1820-Bearbeitet.jpg"),name='Tobias Schoch')
-# dlib_api.teach_person(load_image(r"D:\Users\TOS\Pictures\Camera Roll\WIN_20180412_14_00_22_Pro.jpg"),name='Tobias Schoch')
-
-def is_picture(filename):
-    image_extensions = {'png', 'jpg', 'jpeg', 'gif'}
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in image_extensions
-
-
-def get_all_picture_files(path):
-    files_in_dir = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
-    return [f for f in files_in_dir if is_picture(f)]
-
-
-def remove_file_ext(filename):
-    return splitext(filename.rsplit('/', 1)[-1])[0]
-
-
-def person_to_dict(p, attributes=['id','name','nmeans','code']):
-    return dict(zip(attributes,[str(getattr(p, a)) if a!='code' else str(getattr(p, a).tolist()) for a in attributes]))
-
-# <Picture functions> #
 
 # <Controller>
 
-@app.route('/image/teach')
+@app.route('/image/teach', methods=['POST'])
 def image_teach():
-    pass
+    file = utils.extract_image(request)
+    name, id = utils.extract_name_or_id(request)
+    image = Image.open(file.stream, 'r').convert('RGB')
+    face = dlib_api.teach_person(image,name=name,id=id,weight=request.args.get('weight',1.0))
+    return jsonify(utils.face_to_dict(face))
 
-@app.route('/image/identify')
+@app.route('/image/identify', methods=['POST'])
 def image_identify():
-    pass
+    file = utils.extract_image(request)
+    image = Image.open(file.stream, 'r').convert('RGB')
+    faces = dlib_api.detect_and_identify_faces(np.array(image))
+    return jsonify(utils.faces_to_list(faces))
 
-@app.route('/facecode/teach')
+@app.route('/facecode/teach', methods=['POST'])
 def code_teach():
-    pass
+    code = utils.extract_facecode(request)
+    name, id = utils.extract_name_or_id(request)
+    p = facedb.teach(code,name=name,id=id,weight=request.args.get('weight',1.0))
+    return jsonify(utils.person_to_dict(p))
 
-@app.route('/facecode/identify')
+@app.route('/facecode/identify', methods=['POST'])
 def code_identify():
-    pass
+    code = utils.extract_facecode(request)
+    p = facedb.identify_person(code)
+    return jsonify(utils.person_to_dict(p))
 
+@app.route('/faces', methods=['GET', 'DELETE'])
+def faces():
+    # GET
+    if request.method == 'GET':
+        return jsonify(list(map(utils.person_to_dict,facedb.persons())))
+    elif request.method == 'DELETE':
+        name, id = utils.extract_name_or_id(request)
+        session = facedb.Session()
+        person = facedb.get_person(name=name, id=id, session=session)
+        session.delete(person)
+        session.commit()
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+    # Web App
 class IdentifyForm(FlaskForm):
     file = FileField('File', validators=[])
+
 class TeachForm(FlaskForm):
     file = FileField('File', validators=[])
     name = TextField('Name', validators=[])
@@ -81,15 +78,26 @@ class TeachForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/identify', methods=['GET', 'POST'])
 def web_identify():
+
     form = IdentifyForm(request.form)
 
     if request.method=='POST':
         if len(request.files)>0:
-            file = request.files['file']
+            file = utils.extract_image(request)
 
-            if is_picture(file.filename):
-                pilimage = Image.open(file.stream, 'r').convert('RGB')
-                faces = dlib_api.detect_and_identify_faces(np.array(pilimage))
+            if utils.is_picture(file.filename):
+                try:
+                    pilimage = Image.open(file.stream, 'r').convert('RGB')
+                except Exception as e:
+                    flash("Error: {}".format(e))
+                    return render_template('identify.html', form=form)
+
+                try:
+                    faces = dlib_api.detect_and_identify_faces(np.array(pilimage))
+                except Exception as e:
+                    flash("Error: {}".format(e))
+                    return render_template('identify.html', form=form)
+
                 font = utils.get_font(1024)
 
                 draw = ImageDraw.Draw(pilimage)
@@ -128,10 +136,10 @@ def web_teach():
         if len(request.files)>0:
             file = request.files['file']
 
-            if is_picture(file.filename):
+            if utils.is_picture(file.filename):
                 pilimage = Image.open(file.stream, 'r')
                 try:
-                    faces = dlib_api.teach_person(np.array(pilimage),name=form.name.data,id=form.id.data)
+                    face = dlib_api.teach_person(np.array(pilimage),name=form.name.data,id=form.id.data)
                     flash("Tought this face!")
                 except Exception as e:
                     flash("Error: {}".format(e))
@@ -144,58 +152,8 @@ def web_teach():
     return render_template('teach.html', form=form)
 
 
-# @app.route('/identify', methods=['POST'])
-# def web_recognize():
-#     file = extract_image(request)
-#
-#     if file and is_picture(file.filename):
-#         # The image file seems valid! Detect faces and return the result.
-#         return jsonify(detect_faces_in_image(file))
-#     else:
-#         raise BadRequest("Given file is invalid!")
-
-
-@app.route('/faces', methods=['GET', 'POST', 'DELETE'])
-def web_faces():
-    # GET
-    if request.method == 'GET':
-        return jsonify(list(map(person_to_dict,facedb.persons())))
-    # else:
-    #     # POST/DELETE
-    #     if 'id' not in request.args:
-    #         raise BadRequest("Identifier for the face was not given!")
-    #
-    #     if request.method == 'POST':
-    #         file = extract_image(request)
-    #         try:
-    #             new_encoding = calc_face_encoding(file)
-    #             faces_dict.update({request.args.get('id'): new_encoding})
-    #         except Exception as exception:
-    #             raise BadRequest(exception)
-    #
-    #     elif request.method == 'DELETE':
-    #         faces_dict.pop(request.args.get('id'))
-
-
-
-def extract_image(request):
-    # Check if a valid image file was uploaded
-    if 'file' not in request.files:
-        raise BadRequest("Missing file parameter!")
-
-    file = request.files['file']
-    if file.filename == '':
-        raise BadRequest("Given file is invalid")
-
-    return file
-# </Controller>
-
-
 if __name__ == "__main__":
-    # print("Starting by generating encodings for found images...")
-    # Calculate known faces
-    # faces_dict = get_faces_dict("/root/faces")
 
     # Start app
     print("Starting WebServer...")
-    app.run(host='0.0.0.0', port=8081, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=False)
--- utils.py
+++ utils.py
@@ -1,5 +1,25 @@
 
+from werkzeug.exceptions import BadRequest
 from PIL import Image, ImageFont
+import base64
+import numpy as np
+
+def rect_to_dict(rect):
+    return {side: getattr(rect,side) for side in ['left','right','top','bottom']}
+
+def person_to_dict(p, attributes=['id','name','nmeans','code']):
+    return dict(zip(attributes,[str(getattr(p, a)) if a!='code' else str(getattr(p, a).tolist()) for a in attributes]))
+
+def face_to_dict(face):
+    p, rect, shape = face
+    return {'person':person_to_dict(p), 'position': rect_to_dict(rect)}
+
+def faces_to_list(faces):
+    return [face_to_dict(face) for face in faces]
+
+def is_picture(filename):
+    image_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in image_extensions
 
 def resize_image(image, width=1024):
     basewidth = width
@@ -27,3 +47,32 @@ def get_font(width):
 def draw_rectangle(draw, rect, width, color):
     for i in range(width):
         draw.rectangle([(rect.left()+i, rect.top()+i), (rect.right()-i, rect.bottom()-i)], outline=color)
+
+
+def extract_image(request):
+    # Check if a valid image file was uploaded
+    if 'file' not in request.files:
+        raise BadRequest("Missing file parameter!")
+
+    file = request.files['file']
+    if file.filename == '':
+        raise BadRequest("Given file is invalid")
+
+    return file
+
+
+def extract_facecode(request):
+    if 'code' not in request.args:
+        raise BadRequest("Missing code parameter!")
+    code = np.frombuffer(base64.b64decode(request.args['code']))
+    if len(code.shape) != 1 or code.shape[0] != 128:
+        raise BadRequest("Bad code parameter!")
+    return code
+
+
+def extract_name_or_id(request):
+    name = request.args.get('name',None)
+    id = request.args.get('id',None)
+    if name is None and id is None:
+        raise BadRequest("either ID or NAME must be specified!")
+    return name, id
